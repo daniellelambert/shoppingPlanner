6ff856bf76dd3e157601f36c1a7ea6d894063d72
Saad <sshami@mail.umw.edu>
2013-04-08 19:07:02 -0400

    Integrated View Recipe with HTML/CSS

--- viewRecipe.php
+++ viewRecipe.php
@@ -1,39 +1,176 @@
-<html>
-<body>
 <?php
-#layout still in the works
-  session_start();
-  include "db_connect.php";
-
-  if(isSet($_GET['id'])) {
-	$recipe_id = $_GET['id'];
-	}
+ session_start();
+?>
+<!DOCTYPE html>
+<!-- Website template by freewebsitetemplates.com -->
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Add Recipes - Shopping Planner</title>
+	<link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+<body>
+	<div id="background-lightgreen">
+		background
+	</div>
+	<div class="page">
+		<div class="blog-page">
+			<div class="sidebar">
+				<a href="index.html" id="logo">Shopping <br/>Planner</a>
+				<ul>
+					<li class="home">
+						<a href="index.html">Home</a>
+					</li>
+					<li class="about">
+						<a href="browseRecipes.php">Browse Recipes</a>
+					</li>
+					<li class="projects">
+						<a href="mealPlan.html">Meal Plan</a>
+					</li>
+					<li class="selected blog">
+						<a href="addRecipes.html">Add Recipes</a>
+					</li>
+					<li class="contact">
+						<a href="searchRecipe.php">Search Recipe</a>
+					</li>
+				</ul>
+				<div class="connect">
+					<a href="http://freewebsitetemplates.com/go/facebook/" id="fb">facebook</a> <a href="http://freewebsitetemplates.com/go/twitter/" id="twitter">twitter</a> <a href="http://freewebsitetemplates.com/go/googleplus/" id="googleplus">google+</a> <a href="http://freewebsitetemplates.com/go/youtube/" id="youtube">youtube</a>
+				</div>
+			</div>
+			<div class="body">
+				<div class="content-blog-single">
+					<div>
+						<div>
+							<h3></h3>
+							<div class="paging">
+								
+							</div>
+							<ul>
+								<li>
+									<img src="images/dinner2.png" alt="">
+									
+									<div class="section">
+										<div>
+										
+                                        <?	
+										
+										include "db_connect.php";
+                                        
+										if(isSet($_GET['id'])) {
+											$recipe_id = $_GET['id'];
+										}
 	
-#	$query = "Select recipe.*, ingredient.*, food.* From recipe INNER JOIN ingredient INNER JOIN food ON recipe.recipe_id=ingredient.recipe_id and ingredient.food_id=food.food_id WHERE recipe.recipe_id =$recipe_id";
-	$query = "Select * from recipe where recipe_id=$recipe_id";
-	$result = mysqli_query($db,$query) or die("Error Querying Database");
+										#$query = "Select recipe.*, ingredient.*, food.* From recipe INNER JOIN ingredient INNER JOIN food ON recipe.recipe_id=ingredient.recipe_id and ingredient.food_id=food.food_id WHERE recipe.recipe_id =$recipe_id";
+										$query = "Select * from recipe where recipe_id=$recipe_id";
+										$result = mysqli_query($db,$query) or die("Error Querying Database");
 	
-	if($row = mysqli_fetch_array($result)){
-#	print_r ($row);
-		echo 'Name: '.$row['recipe_name'].'<br/>';
-	echo 'Cook Time: '.$row['cook_time'].'<br/><br/>';
-	echo $row['description'].'<br/><br/>';
+										if($row = mysqli_fetch_array($result)){
+											#print_r ($row);
+											echo '<h3>'.$row['recipe_name'].'</h3><br/>';
+											echo "<h4>Cook Time:</h4>";
+											echo $row['cook_time'].'<br/><br/>';
+											echo "<h4>Description:</h4>";
+											echo $row['description'].'<br/><br/>';
 
-	}	
-	#Get ingredients
-	$ingredientQuery = "SELECT ingredient.*, food.* FROM ingredient INNER JOIN food on ingredient.food_id=food.food_id where ingredient.recipe_id=$recipe_id";
-	$result2 = mysqli_query($db,$ingredientQuery) or die("Error Querying Database");
-	while ($iRow = mysqli_fetch_array($result2)){
-		echo "<br/> <br/>";
+										}	
+										
+										#Get ingredients
+										$ingredientQuery = "SELECT ingredient.*, food.* FROM ingredient INNER JOIN food on ingredient.food_id=food.food_id where ingredient.recipe_id=$recipe_id";
+										$result2 = mysqli_query($db,$ingredientQuery) or die("Error Querying Database");
+										echo "<h4>Ingredients:</h4>";
+										while ($iRow = mysqli_fetch_array($result2)){
+											#print_r ($iRow);
+											echo $iRow['amount'].' '.$iRow['units'].' of '.$iRow['food_name'].'<br/>';
+										}
+										
+										echo "<br/>";
+										echo "<h4>Instructions:</h4>";
+										echo $row['instructions'].'<br/>';
+										echo "<form id='formID' action='modifyRecipe.php' method='POST'> <input type='hidden' name='id' value=".$recipe_id." /> <input type='submit' value='Modify This Recipe!' /> </form><br/>";
+										echo "<form id='formID' action='deleteRecipe.php' method='POST'> <input type='hidden' name='id' value=".$recipe_id." /> <input type='submit' value='Delete This Recipe!' /> </form>";
+										
+                                        
+                                        ?>
+                                            
+                                            
+										</div>
+									</div>
+								</li>
+							</ul>
+						</div>
+						<div class="sidebar">
+							<div>
+							
+							</div>
+							
+							<div>
+								
+								<ul>
+									
+								</ul>
+								<ul>
+									
+								</ul>
+							</div>
+							
+						</div>
+					</div>
+				</div>
+				<div class="footer">
+					<p>
+						
+					</p>
+					<ul>
+						<li>
+							<a href="index.html">Home</a>
+						</li>
+						<li>
+							<a href="browseRecipes.php">Browse Recipes</a>
+						</li>
+						<li>
+							<a href="mealPlan.html">Meal Plan</a>
+						</li>
+						<li>
+							<a href="addRecipes.html">Add Recipes</a>
+						</li>
+						<li>
+							<a href="searchRecipe.php">Search Recipe</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html><script type='text/javascript'>
+/*var mainCheck = document.getElementById('mainCheck');
+var dishChecker = document.createElement('div');
+dishChecker = setAttribute('id', 'dish_checker');
 
-#		print_r ($iRow);
-		echo $iRow['amount'].' '.$iRow['units'].' of '.$iRow['food_name'].'<br/>';
-	}
-	
-		echo $row['instructions'].'<br/>';
-		echo "<form id='formID' action='modifyRecipe.php' method='POST'> <input type='hidden' name='id' value=".$recipe_id." /> <input type='submit' value='Modify This Recipe!' /> </form><br/>";
-		echo "<form id='formID' action='deleteRecipe.php' method='POST'> <input type='hidden' name='id' value=".$recipe_id." /> <input type='submit' value='Delete This Recipe!' /> </form>";
-	?>
+dishChecker.innerHTML = ("");
+mainCheck.appendChild (dishChecker);
+*/
+function mainDishCheck(){
+if (document.getElementById('mainDishCBox').checked){
+document.getElementById('mainDish').style.visibility = 'visible';
+}
+else { 
+document.getElementById('mainDish').style.visibility = 'hidden';
+}
+}
+var j = 1;
+function AddIngredient(){
+var content=document.getElementById('ingredientDiv');
+var newBox=document.createElement('div');
+newBox.setAttribute('id', 'ingredients'+j);
+newBox.innerHTML = ("<table><tr><td><input type='text' style='width:100px;' value='Amount*' id='foodAmount' name='food["+j+"][Amt]'onblur='this.value=!this.value?'Amount *':this.value;' onfocus='this.select()' onclick='this.value='';'></td> <td><input type='text' value='Units*' style='width:100px;' id='Units' name='food["+j+"][Unit]'onblur='this.value=!this.value?'Units *':this.value;' onfocus='this.select()' onclick='this.value='';'></td><td><input type='text' style='width:100px;' value='Of*' id='food_Name' name='food["+j+"][Food_Name]' onblur='this.value=!this.value?'of *':this.value;' onfocus='this.select()' onclick='this.value='';'></td></tr></table>");
+content.appendChild (newBox);
+j++;
 
-	</body>
-	</html>
+												
+
+
+}
+
+</script>
