6ff23791fb6758c725bc4db075e53cbb57c000ad
Saad <sshami@mail.umw.edu>
2013-04-08 19:35:05 -0400

    Integrated Modify Recipe with HTML/CSS & More
    - Along with that I replaced the modifyRecipe2.php page with
    modifyRecipeConfirm.php

---
+++ modifyRecipeConfirm.php
@@ -0,0 +1,196 @@
+<?php
+ session_start();
+?>
+<!DOCTYPE html>
+<!-- Website template by freewebsitetemplates.com -->
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Add Recipes - Shopping Planner</title>
+	<link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+<body>
+	<div id="background-lightgreen">
+		background
+	</div>
+	<div class="page">
+		<div class="blog-page">
+			<div class="sidebar">
+				<a href="index.html" id="logo">Shopping <br/>Planner</a>
+				<ul>
+					<li class="home">
+						<a href="index.html">Home</a>
+					</li>
+					<li class="about">
+						<a href="browseRecipes.php">Browse Recipes</a>
+					</li>
+					<li class="projects">
+						<a href="mealPlan.html">Meal Plan</a>
+					</li>
+					<li class="selected blog">
+						<a href="addRecipes.html">Add Recipes</a>
+					</li>
+					<li class="contact">
+						<a href="searchRecipe.php">Search Recipe</a>
+					</li>
+				</ul>
+				<div class="connect">
+					<a href="http://freewebsitetemplates.com/go/facebook/" id="fb">facebook</a> <a href="http://freewebsitetemplates.com/go/twitter/" id="twitter">twitter</a> <a href="http://freewebsitetemplates.com/go/googleplus/" id="googleplus">google+</a> <a href="http://freewebsitetemplates.com/go/youtube/" id="youtube">youtube</a>
+				</div>
+			</div>
+			<div class="body">
+				<div class="content-blog-single">
+					<div>
+						<div>
+							<h3></h3>
+							<div class="paging">
+								
+							</div>
+							<ul>
+								<li>
+									<img src="images/dinner2.png" alt="">
+									
+									<div class="section">
+										<div>
+										
+
+										<?php
+										
+										#Still working on actually updating the recipe. 
+											include "db_connect.php";
+										#Getting values from form
+										$recipe = $_POST;
+										$rec_id = mysqli_real_escape_string($db, trim($recipe['id']));
+										#string
+										$recipe_name = mysqli_real_escape_string($db, trim($recipe['Dish_Name']));
+										#string
+										$directions = mysqli_real_escape_string($db, trim($recipe['direct']));
+										#string
+										$description =   mysqli_real_escape_string($db, trim($recipe['descript']));
+										#time (maybe date time?
+										$cook_time =  mysqli_real_escape_string($db, trim($recipe['cook_time']));
+										#string
+										$comment = mysqli_real_escape_string($db, trim($recipe['comments']));
+										#number
+										//$sides = mysqli_real_escape_string($db, trim($recipe['sideDish']));
+
+										#Updating recipe with values from form
+										$update_rec = "UPDATE recipe SET recipe_name='".$recipe_name."', description='".$description."', instructions='".$directions."', cook_time='".$cook_time."', comments='".$comment."' WHERE recipe_id = ".$rec_id;
+										
+										 mysqli_query($db, $update_rec) OR DIE (mysqli_error($db));
+										
+										
+										 #Foreach loop to increment over stuff for food/ingredient
+										foreach ($recipe['food'] as $k => $v){
+
+										#check if updating ingredient
+											if (isset($v['food_id'])){
+												
+												#print_r ($v);
+												echo "<br/>";
+												#if checkbox to delete == 'Yes', delete it
+												if (isset($v['delCheck']) && $v['delCheck']=='on'){
+													$delete_Query = "DELETE FROM ingredient where recipe_id =".$rec_id." and ".$v['food_id'];
+													mysqli_query($db, $delete_Query) OR DIE (mysqli_error($db));
+												}
+												else{
+													#Update ingredient and food with new info
+													$update_ingred = "update ingredient set amount=".$v['Amt'].", units='".$v['Unit']."' where recipe_id=".$rec_id." && food_id=".$v['food_id'];
+													$update_food = "update food set food_name='".$v['Food_Name']."' WHERE food_id=".$v['food_id'];
+													mysqli_query($db, $update_ingred) OR DIE ("Here! ".$update_ingred);
+													mysqli_query($db, $update_food) OR DIE ($update_food);
+												}
+											}
+											else{
+												
+												   #print_r ($v);
+												echo "  Else Statement <br/>";
+												#Adding new food. 
+													$food_result = getFood($v['Food_Name'], $db);
+												if ($food_result == null){
+													
+										#if it isn't in the db, add it.
+											 $insert_food =  "INSERT INTO food VALUES (null, '".mysqli_real_escape_string($db, trim($v['Food_Name']))."', 'placeHolder')";
+													 $result_insert_food =mysqli_query($db, $insert_food) OR DIE ("ERROR INSERTING FOOD".$insert_food);
+												if ( $result_insert_food == false){
+													echo "ERROR inserting food";
+												}
+																  #getting food again (for food_id)
+																 $food_result = getFood($v['Food_Name'], $db);
+										 
+														}
+										#Adding ingredient
+										
+										$amt = mysqli_real_escape_string($db, trim($v['Amt']));
+										$unit = mysqli_real_escape_string($db, trim($v['Unit']));
+										$insert_ingredient = "INSERT INTO ingredient VALUES ((select MAX(food.food_id) from food), ".$rec_id.", '$amt', '$unit')";
+										$result_insert_ingredient =mysqli_query($db, $insert_ingredient) OR DIE (mysqli_error($db));
+										 }
+												
+												}
+
+
+										#else (meaning insert need ingredient to the DB
+												#check DB for Food name
+													#if it is
+														#insert into ingredient values ($recipe.name, food_id from DB check, amt, units);
+													#else
+														#insert into food values (null, food[i][food_name], 'placehold')
+														#insert into ingredient values ($recipe.name, (select MAX(food.food_id) from food), amt, units);
+														
+									#redirect back to recipe page
+									echo "<p>The recipe has been modified. <a href=\"viewRecipe.php?id=".$rec_id."\">Click Here</a> to return back to the recipe page.</p>";
+									?>                                        
+                                       
+                                            
+                                            
+										</div>
+									</div>
+								</li>
+							</ul>
+						</div>
+						<div class="sidebar">
+							<div>
+							
+							</div>
+							
+							<div>
+								
+								<ul>
+									
+								</ul>
+								<ul>
+									
+								</ul>
+							</div>
+							
+						</div>
+					</div>
+				</div>
+				<div class="footer">
+					<p>
+						
+					</p>
+					<ul>
+						<li>
+							<a href="index.html">Home</a>
+						</li>
+						<li>
+							<a href="browseRecipes.php">Browse Recipes</a>
+						</li>
+						<li>
+							<a href="mealPlan.html">Meal Plan</a>
+						</li>
+						<li>
+							<a href="addRecipes.html">Add Recipes</a>
+						</li>
+						<li>
+							<a href="searchRecipe.php">Search Recipe</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
---
+++ modifyRecipeOld.php
@@ -0,0 +1,81 @@
+<html>
+<body>
+<?php
+#layout still in the works
+  session_start();
+  include "db_connect.php";
+  #Wasn't sure where to define this so it works with javascript later. Increment is used in the food/ingredients array, to keep the amount, units, and name seperated.
+$increment = 0;
+  if(isSet($_POST['id'])) {
+	$recipe_id = $_POST['id'];
+	}
+	
+#	$query = "Select recipe.*, ingredient.*, food.* From recipe INNER JOIN ingredient INNER JOIN food ON recipe.recipe_id=ingredient.recipe_id and ingredient.food_id=food.food_id WHERE recipe.recipe_id =$recipe_id";
+	$query = "Select * from recipe where recipe_id=".$recipe_id;
+	$result = mysqli_query($db,$query) or die("Error Querying Database");
+	
+	if($row = mysqli_fetch_array($result)){
+	echo "<form id='formID' action='modifyRecipe2.php' method='POST'>";
+#	print_r ($row);
+		echo 'Name: <input type="text" name="Dish_Name" value="'.$row['recipe_name'].'" /><br/>';
+	echo 'Cook Time: <input type="text" name="cook_time" value="'.$row['cook_time'].'" /><br/><br/>';
+	echo "Description: <br/> <textarea form='formID' name='descript'>".$row['description']." </textarea> <br/><br/>";
+
+	}	
+	#Get ingredients
+	
+	$ingredientQuery = "SELECT ingredient.*, food.* FROM ingredient INNER JOIN food on ingredient.food_id=food.food_id where ingredient.recipe_id=$recipe_id";
+	$result2 = mysqli_query($db,$ingredientQuery) or die("Error Querying Database");
+echo "<div id='ingredientDiv'> ";
+$increment = 0;
+	while ($iRow = mysqli_fetch_array($result2)){
+		echo "<br/> <br/>";
+
+#		print_r ($iRow);
+echo "Remove from Recipe: <input type='checkbox' id='foodCheck' name='food[$increment][delCheck]' />";
+ echo "<input type='hidden' name='food[$increment][food_id]' value='".$iRow['food_id']."'/> Amount: <input type='text' id='foodAmount' name='food[$increment][Amt]' value='".$iRow['amount']."' />  Units: <input type='text' id='Units' name='food[$increment][Unit]' value='".$iRow['units']."' />  of: <input type='text' id='food_Name' name='food[$increment][Food_Name]' value='".$iRow['food_name']."' /> <br/>";
+		
+ $increment++;
+	}
+	
+	echo "</div>";
+	echo "<input type='button' value='More Ingredients' onClick='AddIngredient()'> <br/>";
+	echo "Directions: <br/> <textarea form='formID' name='direct'>".$row['instructions']." </textarea> <br/><br/>";
+	echo "Comments: <br/> <textarea form='formID' name='comments'>".$row['comments']." </textarea> <br/><br/>";
+	echo "<input type='hidden' name='id' value='".$recipe_id."'/>";
+	echo "<input type='submit' value='Modify!'/>";
+	echo "</form>";
+	
+	?>
+
+	</body>
+	</html>
+	
+	<script type='text/javascript'>
+/*var mainCheck = document.getElementById('mainCheck');
+var dishChecker = document.createElement('div');
+dishChecker = setAttribute('id', 'dish_checker');
+
+dishChecker.innerHTML = ("");
+mainCheck.appendChild (dishChecker);
+*/
+function mainDishCheck(){
+if (document.getElementById('mainDishCBox').checked){
+document.getElementById('mainDish').style.visibility = 'visible';
+}
+else { 
+document.getElementById('mainDish').style.visibility = 'hidden';
+}
+}
+var j = <?php echo $increment ?>;
+function AddIngredient(){
+var content=document.getElementById('ingredientDiv');
+var newBox=document.createElement('div');
+newBox.setAttribute('id', 'ingredients'+j);
+newBox.innerHTML = ("Remove from Recipe: <input type='checkbox' id='foodCheck' name='food["+j+"][delCheck]' />Amount: <input type='text' id='foodAmount' name='food["+j+"][Amt]' />  Units: <input type='text' id='Units' name='food["+j+"][Unit]' />  of: <input type='text' id='food_Name' name='food["+j+"][Food_Name]' /> <br/>  ");
+content.appendChild (newBox);
+j++;
+
+
+}
+</script>
